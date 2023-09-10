4f600ee153b6057ec11a583ab458953c915260f0
Catherine <catherine.mathenge@strathmore.edu>
2023-09-10 16:32:31 +0300

    Done with assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        # Filter words from word_list that are anagrams of self.word
+        return [word for word in word_list if self.is_anagram(word)]
+
+    def is_anagram(self, other_word):
+        # Check if two words are anagrams
+        return sorted(self.word.lower()) == sorted(other_word.lower())
+
+# Example usage:
+# anagram = Anagram("enlist")
+# matches = anagram.match(["listen", "silent", "hippopotamus"])
+# print(matches)  # Output will be ["listen", "silent"] for this example
